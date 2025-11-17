81118896eb88edd9cf9a63851a6375c59e220848
longfei <longfy94@gmail.com>
2025-11-17 21:39:56 +0800

    xxx

--- real_world_agent.py
+++ real_world_agent.py
@@ -9,14 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SYSTEM_PROMPT = """You are an expert weather forecaster, who speaks in puns.
-
-You have access to two tools:
-
-- get_weather_for_location: use this to get the weather for a specific location
-- get_user_location: use this to get the user's location
-
-If a user asks you for the weather, make sure you know the location. If you can tell from the question that they mean wherever they are, use the get_user_location tool to find their location."""
+SYSTEM_PROMPT = """You are a  helpful weather assistant, who always cracks jokes and is humorous while remaining helpful"""
 
 @tool('get_weather_for_location', description="Get the current weather for a given city.", return_direct=False)
 def get_weather_for_location(city: str) -> dict:
@@ -43,20 +36,14 @@ def get_user_location(runtime: ToolRuntime[Context]) -> str:
 model = init_chat_model(
     model='gpt-4.1-mini',
     temperature=0.5,
-    timeout=10,
-    max_tokens=1000
 )
 
 @dataclass
 class ResponseFormat:
-    """Response schema for the agent."""
-    # A punny response (always required)
-    punnyResponse: str
+    summary: str
     temperature_celsius: float
     temperature_fahrenheit: float
     humidity: float
-    # Any interesting information about the weather if available
-    weather_conditions: str | None = None
 
 
 checkpointer = InMemorySaver()
@@ -64,7 +51,7 @@ checkpointer = InMemorySaver()
 agent = create_agent(
     model=model,
     system_prompt=SYSTEM_PROMPT,
-    tools=[get_user_location, get_weather_for_location],
+    tools=[get_weather_for_location, get_user_location],
     context_schema=Context,
     response_format=ResponseFormat,
     checkpointer=checkpointer
@@ -82,7 +69,7 @@ print(response['structured_response'])
 
 # 采用相同的 thread id 接着进行交流
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "how are you?"}]},
+    {"messages": [{"role": "user", "content": "What you said just now?"}]},
     config=config,
     context=Context(user_id='u123')
 )
